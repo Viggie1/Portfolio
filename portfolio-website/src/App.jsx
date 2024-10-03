dc97795acd5287602179b38fc6dff2f06311b197
Viggie1 <haxshapova@gmail.com>
2024-10-03 12:37:32 -0400

    Fixes layout issues

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -4,20 +4,20 @@ function App() {
 
   return (
     <>
-      <div className='h-screen w-lg justify-center items-center bg-green-700 text-white'>
-        <div className="flex flex-row justify-center">
-          <div className="flex flex-col mt-60 justify-left w-2/3">
-            <h1 className="text-3xl font-bold">Evangelos Kontonotas</h1>
-            <p className="text-xl pt-2 pb-8">Software Engineer</p>
+      <div className='h-screen bg-green-700 text-white'>
+        <div className="flex flex-row justify-center mx-auto w-full max-w-5xl">
+          <div className="flex flex-col justify-start w-3/5 mt-40 px-6">
+            <h1 className="text-4xl font-bold">Evangelos Kontonotas</h1>
+            <p className="text-2xl pt-2 pb-8">Software Engineer</p>
             <Sidebar/>
           </div>
-          <div className="mt-80 w-1/3">
-            <p className="text-align-left">
-              Hello this is my baout me!
-              adslkfjasldkfjalksjfdlksafd
-              asdfkajsdflksajdflkasjfdlkjsaflkjsadlkfjaslkdjfalksdfjalksdjflkasjfdlksadf
-              asdfkljsadlkfjaslkfdjsalkjalkdsjflkasjd
-              adskfljaslkfdjaslkfdjsalkfdjsalkjfdsalkdjfasdlfkjaslkdfjalksdjf
+          <div className="w-2/5 mt-48 px-6">
+            <p className="text-left overflow-auto">
+              <section>Hello I am Evangelos Kontonotas and I am a software engineer</section>
+              <br />
+              <section>I make software like this</section>
+              <br />
+              <section>When I make software I feel happy</section>
             </p>
           </div>
         </div>
