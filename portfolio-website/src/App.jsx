cd02b40d10a2830e81b450c47786c02a5aa476ec
Viggie1 <haxshapova@gmail.com>
2024-10-03 12:28:03 -0400

    Basic layout changes that I want to save. Going to do a bit of a refactor now.

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -4,17 +4,24 @@ function App() {
 
   return (
     <>
-      <div className='h-screen flex flex-row justify-center items-center bg-green-700 text-white'>
-        <div className="flex flex-col">
-          <h1 className="text-xl font-bold">Evangelos Kontonotas</h1>
-          <p className="pt-2 pb-8">hello</p>
-          <Sidebar/>
-        </div>
-        <div className="px-10">
-          <p>Some stupid paragraph</p>
+      <div className='h-screen w-lg justify-center items-center bg-green-700 text-white'>
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-col mt-60 justify-left w-2/3">
+            <h1 className="text-3xl font-bold">Evangelos Kontonotas</h1>
+            <p className="text-xl pt-2 pb-8">Software Engineer</p>
+            <Sidebar/>
+          </div>
+          <div className="mt-80 w-1/3">
+            <p className="text-align-left">
+              Hello this is my baout me!
+              adslkfjasldkfjalksjfdlksafd
+              asdfkajsdflksajdflkasjfdlkjsaflkjsadlkfjaslkdjfalksdfjalksdjflkasjfdlksadf
+              asdfkljsadlkfjaslkfdjsalkjalkdsjflkasjd
+              adskfljaslkfdjaslkfdjsalkfdjsalkjfdsalkdjfasdlfkjaslkdfjalksdjf
+            </p>
+          </div>
         </div>
       </div> 
-
     </>
   )
 }
