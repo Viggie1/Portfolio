b7712179e25635f76ab5fa7eb66adc2a0f3aaa59
Viggie1 <haxshapova@gmail.com>
2024-10-04 00:33:03 -0400

    Changes sidebar component to be sticky and hold all information rather than splitting in App.jsx. Also adds icons for links and properly implements them.

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -7,11 +7,7 @@ function App() {
       <div className='min-h-screen bg-green-700 text-white'>
         <div className="flex flex-row justify-center mx-auto w-full max-w-5xl">
           <div className="flex flex-col justify-start w-3/5 mt-40 px-6">
-            <h1 className="text-4xl font-bold">Evangelos Kontonotas</h1>
-            <p className="text-2xl pt-2 pb-8">Software Engineer</p>
             <Sidebar/>
-            <div className="flex-grow"></div>
-            <nav className="sticky bottom-0">Linkedin Github Email</nav>
           </div>
           <div className="w-2/5 mt-48 px-6">
             <div>
--- portfolio-website/src/Sidebar.jsx
+++ portfolio-website/src/Sidebar.jsx
@@ -1,11 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"; 
+
 function Sidebar() {
     return (
         <>
-            <div>
+            <div className="sticky top-44">
+                <h1 className="text-4xl font-bold">Evangelos Kontonotas</h1>
+                <p className="text-2xl pt-2 pb-8">Software Engineer</p>
                 <ul>
                     <li className="pb-2">About</li>
                     <li className="pb-2">Experience</li>
                     <li className="pb-2">Projects</li>
+                </ul>
+                <ul className="ml-1 mt-48 flex items-center">
+                    <li className="pr-4">
+                        <a href="https://www.linkedin.com/in/evangelos-kontonotas-420ba723b/" target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+                        </a>
+                    </li>
+                    <li className="pr-4">
+                        <a href="https://github.com/Viggie1" target="_blank" rel="noreferrer">
+                            <FontAwesomeIcon icon={faGithub} size="2x"/>
+                        </a>
+                    </li>
+                    <li className="pr-4">
+                        <a href="mailto:evangelos.kontonotas@gmail.com">
+                            <FontAwesomeIcon icon={faEnvelope} size="2x" />
+                        </a>
+                    </li>
                 </ul>
             </div> 
         
